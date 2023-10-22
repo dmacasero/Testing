eb3e0d8f686b319935e640d894f1b17639258fec
Dominique Kevin Macasero <dominique@abstract.ph>
2023-10-22 18:51:47 +0800

    Added image integration

--- src/app/components/img-container/img-container.component.ts
+++ src/app/components/img-container/img-container.component.ts
@@ -43,7 +43,7 @@ export class ImgContainerComponent implements OnInit {
       data: [
         {
           header: `Answer your Body's Needs`,
-          text: '',
+          text: `The way ingredients are sourced affects the way we nourish our bodies. Author Mark Schatzer believes our body naturally devolops an appetite for the foods and nutrients it needs to be healthy, but that artificial flavourings are getting in the way. This can be reversed by focusing on high-quality ingredients and being mindful as your appetite guides you to consume according to your body's needs.`,
           type: 'header',
           img: '',
           imgX2: '',
