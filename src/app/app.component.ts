1583986802ffb27b29be38a909973adf40b4411e
Dominique Kevin Macasero <dominique@abstract.ph>
2023-10-29 10:21:08 +0800

    Added anchor tag click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'skills_test';
+
+  constructor() {
+    this.registerClickEvent();
+  }
+
+  registerClickEvent() {
+    addEventListener('click', async (ev: Event) => {
+      if (!ev.target) {
+        return;
+      }
+      if (!(ev.target instanceof Element)) {
+        return;
+      }
+
+      const origin = ev.target.closest('a');
+      if (!origin || !origin.href) {
+        return;
+      }
+
+      console.log(`You clicked ${origin.href} which is an anchor tag!`);
+    });
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,10 +4,9 @@ import { CardContent } from 'src/app/components/img-card/typings';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  
   cards: CardContent[] = [
     {
       img: 'assets/component-02/Image-01.jpg',
@@ -27,11 +26,9 @@ export class HomeComponent implements OnInit {
       header: 'Summer Lunch Menu By Mark Best',
       text: 'AEG ambasaddor Mark Bests summer eats are guaranteed to help you make the most of the warmer weather and entertaining at home.',
     },
-  ]
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
